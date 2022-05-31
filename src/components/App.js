06a2b51fa8fb85fcd706eea2eee03bbd4f1ed652
nujeyh <dza456@naver.com>
2022-05-31 14:49:52 +0900

    Add Font

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import styled from "styled-components";
 
 import Main from "../routes/Main";
 import UpdateWord from "../routes/UpdateWord";
@@ -8,15 +9,19 @@ import Header from "./Header";
 
 function App() {
   return (
-    <>
+    <AppWrap>
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/word/:id" element={<UpdateWord />} />
-        <Route path="*" element={<h2>Not Found</h2>} />
+        <Route path="*" element={<h2>⛔️ !잘못된 주소에요! ⛔️</h2>} />
       </Routes>
-    </>
+    </AppWrap>
   );
 }
+
+const AppWrap = styled.div`
+  font-family: "Noto Serif KR", serif;
+`;
 
 export default App;
