69b73335769108709c41231a5cbdacc5bb3a5c98
nujeyh <dza456@naver.com>
2022-05-31 14:52:04 +0900

    Add Floating button

--- src/components/FloatingBtn.js
+++ src/components/FloatingBtn.js
@@ -1,14 +1,47 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import UpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 const FloatingBtn = () => {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <Link to="/word/new">
-        <button>새 단어</button>
-      </Link>
-    </>
+    <FloatingWrap>
+      <div>
+        <Fab
+          color="primary"
+          aria-label="add"
+          onClick={() => {
+            window.scroll({ top: 0, left: 0, behavior: "smooth" });
+          }}
+        >
+          <UpIcon />
+        </Fab>
+      </div>
+      <Fab
+        color="primary"
+        aria-label="add"
+        onClick={() => navigate("/word/new")}
+      >
+        <AddIcon />
+      </Fab>
+    </FloatingWrap>
   );
 };
+
+const FloatingWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  div {
+    margin-bottom: 5px;
+  }
+`;
 
 export default FloatingBtn;
