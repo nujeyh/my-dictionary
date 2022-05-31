55fb01f755719fd0eecc094bb244d75e6f4131a3
nujeyh <dza456@naver.com>
2022-05-31 14:52:19 +0900

    Add Animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+import TypeIt from "typeit-react";
 
 const Header = () => {
   return (
-    <header>
-      <h1>나만의 사전</h1>
-    </header>
+    <HeaderWrap>
+      <h1>
+        <TypeIt>나만의 사전</TypeIt>
+      </h1>
+      <hr />
+    </HeaderWrap>
   );
 };
+
+const HeaderWrap = styled.header`
+  text-align: center;
+  hr {
+    margin: 10px 0 20px 0;
+  }
+`;
 
 export default Header;
