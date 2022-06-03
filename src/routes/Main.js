a375dc7f0b7436a16ffc407cd4110e00a57b4387
nujeyh <102746846+nujeyh@users.noreply.github.com>
2022-06-03 09:40:59 +0900

    Update Main.js

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -10,6 +10,7 @@ import Word from "../components/Word";
 const Main = () => {
   const dispatch = useDispatch();
 
+  // 파이어 베이스 -> 리덕스 데이터 불러오기
   useEffect(() => {
     dispatch(loadDictFB());
   }, []);
@@ -33,6 +34,7 @@ const Main = () => {
   );
 };
 
+// grid로 반응형 페이지 만들기
 const MainContainer = styled.div`
   margin: 0 20px;
   display: grid;
